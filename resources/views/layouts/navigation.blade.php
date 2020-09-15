d935efa9739a662e58a77758f64bba1224af5f05
Asip Hamdi <asiphamdi13@gmail.com>
2020-09-15 14:13:21 +0700

    initial

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,43 @@
+<ul class="sidebar-menu">
+    <li class="header"><strong>MAIN NAVIGATION</strong></li>
+    <li>
+        <a href="{{ route('home') }}">
+            <i class="icon icon-dashboard2 blue-text s-18"></i> 
+            <span>Dashboard</span>
+        </a>
+    </li>
+    @can('master-role')
+    <li class="header light"><strong>MASTER ROLES</strong></li>
+    <li>
+        <a href="{{ route('master-role.role.index') }}">
+            <i class="icon icon-key4 amber-text s-18"></i> 
+            <span>Role</span>
+        </a>
+    </li>
+    <li class="no-b">
+        <a href="{{ route('master-role.permission.index') }}">
+            <i class="icon icon-clipboard-list2 text-success s-18"></i> 
+            <span>Permission</span>
+        </a>
+    </li>
+    <li class="no-b">
+        <a href="{{ route('master-role.pengguna.index') }}">
+            <i class="icon icon-users blue-text s-18"></i> 
+            <span>Pengguna</span>
+        </a>
+    </li>
+    @endcan
+    <li class="header light"><strong>MASTER JENIS</strong></li>
+    <li>
+        <a href="{{ route('master-jenis.jenisLapak.index') }}">
+            <i class="icon icon-store_mall_directory text-danger s-18"></i> 
+            <span>Jenis Lapak</span>
+        </a>
+    </li>
+    <li class="no-b">
+        <a href="{{ route('blank-page') }}">
+            <i class="icon icon-settings text-success s-18"></i> 
+            <span>Jenis Usaha</span>
+        </a>
+    </li>
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,84 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+
+    /**
+     * Role super-admin
+     */
+    Route::group(['middleware' => ['auth', 'role:super-admin']], function () {
+        /**
+         * Master Roles
+         */
+        Route::prefix('master-roles')->namespace('MasterRole')->name('master-role.')->group(function () {
+            // Role
+            Route::resource('role', 'RoleController');
+            Route::prefix('role')->name('role.')->group(function () {
+                Route::post('api', 'RoleController@api')->name('api');
+                Route::get('{id}/addPermissions', 'RoleController@permission')->name('addPermissions');
+                Route::post('storePermissions', 'RoleController@storePermission')->name('storePermissions');
+                Route::get('{id}/getPermissions', 'RoleController@getPermissions')->name('getPermissions');
+                Route::delete('{name}/destroyPermission', 'RoleController@destroyPermission')->name('destroyPermission');
+            });
+            // Permission
+            Route::resource('permission', 'PermissionController');
+            Route::post('permission/api', 'PermissionController@api')->name('permission.api');
+            // Pegawai
+            Route::resource('pengguna', 'PenggunaController');
+            Route::post('pengguna/api', 'PenggunaController@api')->name('pengguna.api');
+            Route::get('pengguna/{id}/editPassword', 'PenggunaController@editPassword')->name('pengguna.editPassword');
+            Route::post('pengguna/{id}/updatePassword', 'PenggunaController@updatePassword')->name('pengguna.updatePassword');
+        });
+
+        /**
+         * Config Template
+         */
+        Route::prefix('config-templates')->namespace('ConfigTemplate')->name('config-template.')->group(function () {
+            // Template
+            Route::resource('template', 'TemplateController');
+            Route::post('template/api', 'TemplateController@api')->name('template.api');
+        });
+    });
+
+    /**
+     * Page Not Found
+     */
+    Route::get('blank-page', 'BlankPageController@index')->name('blank-page');
+
+
+    /**
+     * Profile
+     */
+    Route::prefix('profile')->namespace('Profile')->name('master-profile.')->group(function () {
+        // Profile
+        Route::resource('profile', 'ProfileController');
+        Route::get('profile/{id}/edtiPasswrod', 'ProfileController@editPassword')->name('profile.editPassword');
+        Route::post('profile/{id}/updatePassword', 'ProfileController@updatePassword')->name('profile.updatePassword');
+    });
+
+    /**
+     * Master Jenis
+     */
+    Route::prefix('Master-Jenis')->namespace('MasterJenis')->name('master-jenis.')->group(function () {
+        // Jenis Lapak
+        Route::resource('jenisLapak', 'JenisLapakController');
+        Route::post('jenisLapak/api', 'JenisLapakController@api')->name('jenisLapak.api');
+    });
+});
